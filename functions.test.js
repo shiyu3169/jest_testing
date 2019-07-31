24937ea6e8a547e56851f2bdd9c8b2409cdc8f4c
Shiyu Wang <shiyu3169@gmail.com>
2019-07-31 10:32:08 -0400

    null test

--- functions.test.js
+++ functions.test.js
@@ -1,9 +1,21 @@
 const functions = require("./functions");
 
+// To be test
 test("Adds 2 + 2 to equal 4", () => {
   expect(functions.add(2, 2)).toBe(4);
 });
 
+// Not to be test
 test("Adds 2 + 2 to NOT equal 5", () => {
   expect(functions.add(2, 2)).not.toBe(5);
+});
+
+// Check for truthy & falsy values
+// toBeNull matches only null
+// toBeUndefined matches only undefined
+// toBeDefined is the opposite of toBeUndefined
+// toBeTruthy matches anything that an if statement treats as true
+// toBeFalsy matches anything that an if statement treats as false
+test("Adds 2 + 2 to NOT equal 5", () => {
+  expect(functions.isNull()).toBeNull();
 });
